ba1ff487daf5b23969854f8a46dc3afb5807fd50
AlassaneDoumbia <doumbialassane10@gmail.com>
2024-07-13 13:26:35 +0000

    correctif on prod url  and date liste product

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   url: '.',
-  api: 'https://fidelity-dev.aplikatechnology.com/ecommerce/api/v1'
-  // api: 'http://51.77.195.228:8590/ecommerce/api/v1'
+  // api: 'https://fidelity-dev.aplikatechnology.com/ecommerce/api/v1'
+  api: 'https://api.fidelity-market.com/ecommerce/api/v1'
 };
