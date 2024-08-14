9289b3be0de2c3fc2f0802f0bb2160f35aacce12
sogore <sogoredev@gmail.com>
2024-08-14 10:05:11 +0000

    Correction de la partie tel

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,4 +5,5 @@ export const environment = {
   // api: 'http://51.77.195.228:8590/ecommerce/api/v1'
 
   // api: 'https://api.fidelity-market.com/ecommerce/api/v1'
+  // api: 'https://api.fidelity-market.com/ecommerce/api/v1/users/reset-forgoten-password?username=22390909090'
 };
