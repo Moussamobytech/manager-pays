1f4f04380d6fb027796da0e3cd9cd46528eefd3f
GAUSST <gaoussoudiarra658@gmail.com>
2025-06-08 15:37:01 +0100

    card layout bug on small sizes fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   url: '.',
-   api: 'http://127.0.0.1:8590/ecommerce/api/v1'
+   api: 'http://127.0.0.1:8595/ecommerce/api/v1'
   // api: 'http://51.77.195.228:8590/ecommerce/api/v1'
   // api: 'https://dev.fidelity-market.com/ecommerce/api/v1'
 
